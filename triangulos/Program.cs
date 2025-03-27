dbb7f2f4ebae19c37d2b3e65b01f0f7de70fa91f
Juan Patricio da Silva Borges <juanpatriciodasilvaborges1@gmail.com>
2025-03-27 13:46:54 -0300

    Classificação de triângulos

---
+++ triangulos/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Digite o primeiro lado do triângulo: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo lado do triângulo: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o terceiro lado do triângulo: ");
+double n3 = Convert.ToDouble(Console.ReadLine());
+
+if(n1 + n2 > n3 && n2 + n3 > n1 && n3 + n1 > n2)
+{
+    if(n1 == n2 && n2 == n3)
+    {
+        Console.WriteLine("Esse triângulo é Equilátero");
+    }
+
+    else if(n1 == n2 || n2 == n3 || n3 == n1)
+    {
+        Console.WriteLine("Esse triângulo é Isósceles");
+    }
+
+    else
+    {
+        Console.WriteLine("Esse triângulo é Escaleno");
+    }
+}
+
+else
+{
+    Console.WriteLine("Os valores fornecidos não formam um triângulo");
+}
